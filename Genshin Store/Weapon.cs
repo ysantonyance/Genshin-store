a2c6b7d625341cdb9090e1e21ac7ffe83d8adbf8
Alice <alis.n1512@gmail.com>
2025-12-09 01:49:15 +0200

    Weapon.cs

--- Genshin Store/Weapon.cs
+++ Genshin Store/Weapon.cs
@@ -1,66 +1,64 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Xml.Linq;
+﻿using System; //подключаем базовые классы
+using System.Collections.Generic; //это для доступа к коллекциям
+using System.Linq; //для Linq методов
+using System.Text; //для работы со строками
+using System.Threading.Tasks; //пространство имён, которое работает с асинхронными задачами
+using System.Xml.Linq; //для работы с Xml
 
-namespace Genshin_Store
+namespace Genshin_Store //пространство имен проекта
 {
-    public class Weapon : InventoryItem, IWishable, IPurchasable
+    public class Weapon : InventoryItem, IWishable, IPurchasable //Weapon наследует InventoryItem и реализует интерфейсы IWishable и IPurchasable
     {
-        private string name;
+        private string name; //приватное поле имени
 
-        public string Name
+        public string Name //и публичное с прроверкой
         {
             get
             {
-                return name;
+                return name; //возвращает имя оружия
             }
             set
             {
-                if (value.Length > 40)
-                    throw new ArgumentException("The name is too long");
-                name = value;
+                if (value.Length > 40) //проверяем длину имени оружия 
+                    throw new ArgumentException("The name is too long"); //если слишком длинное выводим ошибку
+                name = value; //сохраняем.....
             }
         }
 
-        private int rarity;
-
-        public int Rarity
+        private int rarity; //приватное поле для редкости оружия
+        public int Rarity //публичное с проверкой
         {
             get
             {
-                return rarity;
+                return rarity; //возвращаем редкрсть оружия
             }
             set
             {
-                if (value != 3 && value != 4 && value != 5)
-                    throw new ArgumentException($"Weapons can't be {value} star/s");
-                rarity = value;
+                if (value != 3 && value != 4 && value != 5) //проверяем к какой редкости относиться
+                    throw new ArgumentException($"Weapons can't be {value} star/s"); //ошибка если нет такоц редкости
+                rarity = value; //сохраняем.......
             }
         }
 
-        private string type;
-
-        public string Type
+        private string type; //приватное поле для типа оружия
+        public string Type //публичное с проверкой 
         {
             get
             {
-                return type;
+                return type; //возвращаем тип оружия
             }
             set
             {
-                if (value != "Sword" && value != "Claymore" && value != "Polearm" &&
+                if (value != "Sword" && value != "Claymore" && value != "Polearm" && //проверка типа оружия
                     value != "Bow" && value != "Catalyst")
-                    throw new ArgumentException($"Weapons can't be {value} type");
-                type = value;
+                    throw new ArgumentException($"Weapons can't be {value} type"); //если такого типа нет то выводится ошибка
+                type = value; //сохраняем...
             }
         }
 
-        public Weapon(string name, int rarity, string type)
+        public Weapon(string name, int rarity, string type) //конструктор
         {
-            Name = name;
+            Name = name; //устанавливаем имя, редкость и тип оружия
             Rarity = rarity;
             Type = type;
         }
@@ -82,14 +80,13 @@ namespace Genshin_Store
             return Rarity;
         }*/
 
-        public override string ToString()
+        public override string ToString() //для вывода информации об оружии
         {
-            return $"{Name} ({Rarity}* {Type})";
+            return $"{Name} ({Rarity}* {Type})"; //вывод строки
         }
 
         private int price;
-
-        public int Price
+        public int Price //расчет цены в зависимости от редкости
         {
             get
             {
@@ -98,36 +95,34 @@ namespace Genshin_Store
             }
         }
 
-        public bool IsCharacter()
+        public bool IsCharacter() //метод из интерфейса IWishable 
         {
-            return false;
+            return false; //это оружие поэтому это ложь
         }
 
-        private string currencyType = "Starglitter";
-
-        public string CurrencyType
+        private string currencyType = "Starglitter"; //валюта
+        public string CurrencyType //свойство для валюты
         {
             get
             {
-                return currencyType;
+                return currencyType; //возвращаем валюту 
             }
         }
 
-        public bool CanPurchase(Player player)
+        public bool CanPurchase(Player player) //проверка может ли плейер купить оружие
         {
-            return player.GetStarglitter() >= Price;
+            return player.GetStarglitter() >= Price; //если достаточно денег тогда тру
         }
 
-        public void Purchase (Player player)
+        public void Purchase (Player player) //метод покупки
         {
-            if (!CanPurchase(player))
+            if (!CanPurchase(player)) //если нельзя купить тогда будет ошибка
                 throw new ArgumentException("Can't buy weapon");
 
-            player.SetStarglitter(player.GetStarglitter() - Price);
-            player.AddWeapon(this);
-            Console.WriteLine($"You bought {this} for {Price} Starglitter");
+            player.SetStarglitter(player.GetStarglitter() - Price); //в ином случае списываем валюту
+            player.AddWeapon(this); //и добавляем оружие игроку
+            Console.WriteLine($"You bought {this} for {Price} Starglitter"); //выводим сообщение об успешности покупки
         }
-
-
     }
 }
+
