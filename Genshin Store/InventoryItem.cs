33d0247822305e1116619d0a2c0a807f1ef2db4f
Alice <alis.n1512@gmail.com>
2025-12-09 01:13:53 +0200

    InventoryItem.cs

--- Genshin Store/InventoryItem.cs
+++ Genshin Store/InventoryItem.cs
@@ -1,55 +1,98 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System; //подключаем базовые классы
+using System.Collections.Generic; //это для доступа к коллекциям
+using System.Linq; //для Linq методов
+using System.Text;  //для работы со строками
+using System.Threading.Tasks; //пространство имён, которое работает с асинхронными задачами
 
-namespace Genshin_Store
+namespace Genshin_Store //пространство имён для проекта
 {
-    public abstract class InventoryItem : IComparable<InventoryItem>
+    public abstract class InventoryItem : IComparable<InventoryItem> //абстрактный класс InventoryItem, от которого будут наследоваться все предметы
+    //реализует интерфейс IComparable для сортировки предметов
     {
-        private string name;
-        public string Name 
+        private string name; //приватное поле для имени предмета
+        public string Name //и публичное
         {
-            get {  return name; }
-            set { name = value; }
+            get {  return name; } //возвращает имя предмета
+            set { name = value; } //устанавливает новое имя
         }
 
-        private int rarity;
-        public int Rarity
+        private int rarity; //приватное поле для редкости предмета
+        public int Rarity //т публичное
         {
-            get { return rarity;}
-            set { rarity = value; }
+            get { return rarity;}  //возвращает редкость предмета
+            set { rarity = value; }  //устанавливает новую редкость
         }
 
-        public static InventoryItem operator +(InventoryItem a, InventoryItem b)
+        private int quantity = 1; //для кол-во предметов в инвенатре
+        public int Quantity
         {
-            return a + b;
+            get { return quantity; } //возвращает колво 
+            set { quantity = value; }//устанавливает колво
         }
 
-        public static bool operator ==(InventoryItem a, InventoryItem b)
+        protected InventoryItem(string name, int rarity, int quantity = 1) //при создании обьекта вызывается класс, который задает обьекту редкость, имя и колво
         {
-            return a?.Name == b?.Name && a?.Rarity == b?.Rarity;
+            Name = name;
+            Rarity = rarity;
+            Quantity = quantity;
         }
 
-        public static bool operator !=(InventoryItem a, InventoryItem b)
+        public abstract void Use(); //абстрактный класс, который определяет что именно произойдет если использовать предмет
+
+        //public static InventoryItem operator +(InventoryItem a, InventoryItem b)
+        //{
+          //  return a + b;
+        //}
+
+        public static bool operator ==(InventoryItem a, InventoryItem b) //перегрузка оператора == 
         {
-            return !(a == b);
+            return a?.Name == b?.Name && a?.Rarity == b?.Rarity; //сравниваем имя и редкость
         }
 
-        public static bool operator >(InventoryItem a, InventoryItem b)
+        public static bool operator !=(InventoryItem a, InventoryItem b) //перегрузка оператора !=
         {
-            return a.Rarity > b.Rarity;
+            return !(a == b); //возвращаем значение == 
         }
 
-        public static bool operator <(InventoryItem a, InventoryItem b)
+        public static bool operator >(InventoryItem a, InventoryItem b) //перегрузка оператора >
         {
-            return a.Rarity < b.Rarity;
+            return a.Rarity > b.Rarity; //будет истинной если первый предмет больше
         }
 
-        public int CompareTo(InventoryItem other)
+        public static bool operator <(InventoryItem a, InventoryItem b) //перегрузка оператора <
         {
-            return Rarity.CompareTo(other.Rarity);
+            return a.Rarity < b.Rarity; //будет истинной если первый предмет будет меньше
+        }
+
+        public override bool Equals(object obj) //для сравнения предметов
+        {
+            InventoryItem other = obj as InventoryItem; //приводим обьект к инвентарь айтем 
+            if (other == null) return false; //если не выходит то false
+            return this.Name == other.Name && this.Rarity == other.Rarity; //сравниваем имя и редкость
+        }
+
+        public override int GetHashCode() //для использования  вколлекциях
+        {
+            return Name.GetHashCode() + Rarity.GetHashCode(); //генерируем имя и редкость
+        }
+
+        public int CompareTo(InventoryItem other) //сортировка предметов по редкости
+        {
+            if (other == null) return 1; //если второй обьект нулл, то текущий обьект больше
+            return Rarity.CompareTo(other.Rarity); //сравниваем...
+        }
+        
+        public void AddQuantity(int amount) //метод для увеличение колво предметов
+        {
+            if (amount > 0)
+                Quantity += amount; //увеличиваем...
+        }
+        
+        public void RemoveQuantity(int amount) //метод для уменьшения колво предметов
+        {
+            if (amount > 0 && amount <= Quantity)
+                Quantity -= amount; //уменьшаем...
         }
     }
 }
+
